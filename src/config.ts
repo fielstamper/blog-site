a8605f1c411553bb5a35f172cfb69b66737c6230
fielstamper <161728157+fielstamper@users.noreply.github.com>
2026-08-29 02:03:24 +0800

    og:title changes

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "blog.caitlyn.moe",
+	title: "My Blog",
 	subtitle: "",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
