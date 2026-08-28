b671dbe67eb0948da4c130e1a66f07ae1297ec2f
fielstamper <161728157+fielstamper@users.noreply.github.com>
2026-08-29 00:25:47 +0800

    #period

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "alice",
-	bio: "(aka caitlyn). \n\n  A woman who can't program but can do some other stuff decently well. You can find me around the web. ",
+	bio: "(aka caitlyn) \n\n  A woman who can't program but can do some other stuff decently well. You can find me around the web. ",
 	links: [
 				{
 			name: "GitHub",
